9619b26111cf22f2b7ad9ecff8ae06f2aee40753
JTilley24 <justin.tilley@me.com>
2013-04-12 16:37:24 -0400

    Initial project setup

---
+++ AOC1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC1
+//
+//  Created by Justin Tilley on 4/12/13.
+//  Copyright (c) 2013 Justin Tilley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
