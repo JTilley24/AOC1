e1f022affa6a29a4129f5930321cc962b9baf16f
JTilley24 <justin.tilley@me.com>
2013-04-18 14:40:11 -0400

    Added author and published labels

--- AOC1/ViewController.h
+++ AOC1/ViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+    UILabel *title;
+    UILabel *author1;
+    UILabel *author2;
+    UILabel *publish1;
+    UILabel *publish2;
+}
 @end
--- AOC1/ViewController.m
+++ AOC1/ViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad
 {
     self.view.backgroundColor = [UIColor whiteColor];
-    UILabel *title =[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320.0f, 20.0f)];
+    
+    title =[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320.0f, 20.0f)];
     if(title != nil)
     {
         title.backgroundColor = [UIColor greenColor];
@@ -25,6 +26,48 @@
         title.textAlignment = UITextAlignmentCenter;
     }
     [self.view addSubview:title];
+    
+    author1 =[[UILabel alloc] initWithFrame:CGRectMake(0, 20.0f, 100.0f, 20.0f)];
+    if(author1 != nil)
+    {
+        author1.backgroundColor = [UIColor redColor];
+        author1.text = @"Author:";
+        author1.textAlignment = UITextAlignmentRight;
+        author1.textColor = [UIColor blueColor];
+    }
+    [self.view addSubview:author1];
+    
+    author2 = [[UILabel alloc] initWithFrame:CGRectMake(100.0f, 20.0f, 220.0f, 20.0f)];
+    if(author2 != nil)
+    {
+        author2.backgroundColor = [UIColor grayColor];
+        author2.text = @"Richard Marinick";
+        author2.textAlignment = UITextAlignmentLeft;
+        author2.textColor = [UIColor cyanColor];
+    }
+    [self.view addSubview:author2];
+    
+    publish1 =[[UILabel alloc] initWithFrame:CGRectMake(0, 40.0f, 100.0f, 20.0f)];
+    if(publish1 != nil)
+    {
+        publish1.backgroundColor = [UIColor yellowColor];
+        publish1.text = @"Published:";
+        publish1.textAlignment = UITextAlignmentRight;
+        publish1.textColor = [UIColor purpleColor];
+    }
+    [self.view addSubview:publish1];
+    
+    
+    publish2 =[[UILabel alloc] initWithFrame:CGRectMake(100.0f, 40.0f, 220.0f, 20.0f)];
+    if(publish2 != nil)
+    {
+        publish2.backgroundColor = [UIColor blueColor];
+        publish2.text = @"August 25, 2005";
+        publish2.textAlignment = UITextAlignmentLeft;
+        publish2.textColor = [UIColor orangeColor];
+    }
+    [self.view addSubview:publish2];
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
