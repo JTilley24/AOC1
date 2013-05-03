191762cf6f3d14a10339190be3567f6e3e8334e4
JTilley24 <justin.tilley@me.com>
2013-05-02 20:17:54 -0400

    Added login button and label

--- AOC1/ViewController.h
+++ AOC1/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 {
+    UITextField *userText;
+    UILabel *notice;
+    
 }
 
 
--- AOC1/ViewController.m
+++ AOC1/ViewController.m
@@ -29,15 +29,46 @@
     
     
     
-    UITextField *userText = [[UITextField alloc] initWithFrame:CGRectMake(110.0f, 10.0f, 200.0f, 30.0f)];
+    userText = [[UITextField alloc] initWithFrame:CGRectMake(110.0f, 10.0f, 200.0f, 30.0f)];
     if(userText != nil)
     {
         userText.borderStyle =UITextBorderStyleRoundedRect;
         [self.view addSubview:userText];
     }
     
+    UIButton *login =  [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if(login != nil)
+    {
+        login.frame = CGRectMake(230.0f, 50.0f, 80.0f, 30.0f);
+        [login setTitle:@"Login" forState:UIControlStateNormal];
+        [login addTarget:self action:@selector(onClick) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:login];
+    }
+    
+    notice = [[UILabel alloc] initWithFrame:CGRectMake(0, 100.0f, 320.0f, 70.0f)];
+    if(notice != nil)
+    {
+        notice.textAlignment = UITextAlignmentCenter;
+        notice.textColor = [UIColor blueColor];
+        notice.backgroundColor = [UIColor grayColor];
+        [self.view addSubview:notice];
+    }
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)onClick
+{
+    if(userText.text == nil)
+    {
+        notice.text = @"Please Enter Username";
+    }
+    if(userText.text > 0)
+    {
+        notice.text = @"User: has been logged in";
+    }
 }
 
 - (void)didReceiveMemoryWarning
